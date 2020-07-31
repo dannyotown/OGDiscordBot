a66de007a103be27e7566429b1ea184ec55e4f26
Dtown <dannyotown@gmail.com>
2020-07-31 12:58:04 -0500

    Hello World for Heroku Upload

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 
 bot.on("message", (msg) => {
-  console.log(msg);
+  if (msg.content == "!Hello") {
+    msg.reply("Hello World!");
+  }
 });
 
 bot.login(process.env.Discord_Token);
